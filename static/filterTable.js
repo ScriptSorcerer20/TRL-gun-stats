6202b5394d66dc882a2268da4b5c48cb4ba42618
IM23a-spirgif <spirgif@bzz.ch>
2025-05-17 18:01:01 +0200

    Update UI, improve navigation, and refine About Us content

--- static/filterTable.js
+++ static/filterTable.js
@@ -3,7 +3,6 @@ function filterTable() {
     const filter = input.value.toLowerCase();
     const table = document.getElementById('gun-stats');
     const tr = table.getElementsByTagName('tr');
-
     for (let i = 1; i < tr.length; i++) {
         const td = tr[i].getElementsByTagName('td')[1];
         if (td) {
